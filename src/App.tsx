3e4a864f262295380d931db7d30c19efe4530bc7
Akshat Bhardwaj <akshatbhardwaj24@gmail.com>
2024-03-08 18:35:53 +0530

    reinstated the original project

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
-import './globals.css';
+import AuthLayout from "./_auth/AuthLayout";
+import SigninForm from "./_auth/forms/SigninForm";
+import SignupForm from "./_auth/forms/SignupForm";
+import RootLayout from "./_root/RootLayout";
+import { Home } from "./_root/pages";
+import "./globals.css";
+import { Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
-    <div className='text-3xl font-bold underline'>Hello workhive</div>
-  )
-}
+    <main className="flex h-screen">
+      <Routes>
+        {/* Public Routes */}
+        <Route element={<AuthLayout />}>
+          <Route path="sign-in" element={<SigninForm />}></Route>
+          <Route path="sign-up" element={<SignupForm />}></Route>
+        </Route>
 
-export default App
+        {/* Private Routes */}
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />}></Route>
+        </Route>
+      </Routes>
+    </main>
+  );
+};
+
+export default App;
---
+++ src/_auth/forms/SignupForm.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button"
+
+const SignupForm = () => {
+  return (
+    <div>
+      <Button className="flex ">Click Me</Button>
+    </div>
+    
+  )
+}
+
+export default SignupForm
